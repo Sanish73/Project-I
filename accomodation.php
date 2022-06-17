ad1523e2109aee8796168c6ded7dea421ec7f70f
Sanish Thapa <thapasanish73@gmail.com>
2022-06-17 08:51:23 +0545

    hello sanish

--- accomodation.php
+++ accomodation.php
@@ -2,7 +2,7 @@
 
 if(session_status() == PHP_SESSION_NONE)
 {
-	session_start();//start session if session not start
+	session_start();//start session if session not start --
 }
 
 if(isset($_SESSION['departure_date'])){
